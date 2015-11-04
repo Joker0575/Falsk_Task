ff6887a646755d90d64e30b9372ef1ba722dfc72
joker0575 <raju.msit05@gmail.com>
2015-11-05 01:11:56 +0530

    table data added

--- GameLoftTask.py
+++ GameLoftTask.py
@@ -5,13 +5,14 @@ from flask import request
 from flask import request, jsonify
 from flask.ext.mongoengine import MongoEngine
 from textblob import TextBlob
+import time
 
 
 app = Flask(__name__)
 
 
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'twitter_analysis',
+    'db': 'gomeloft_db',
     'host': 'localhost',
     'port': 27017
 }
@@ -21,7 +22,9 @@ db = MongoEngine(app)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User(email="abc",password='rajkoti05@gmail.com ')
+        email = request.form['email']
+        password = request.form['password']
+        user = User(email=email,password=password)
         user.save()
         return redirect("/application", code=302)
     return render_template('index.html')
@@ -29,15 +32,25 @@ def login():
 
 @app.route('/application',methods=['GET', 'POST'])
 def application():
+    text = Data.objects
     if request.method == 'POST':
-        data = Data(subject="test",text_data='rajkoti05@gmail.com ')
+        subject = request.form['subject']
+        text_data = request.form['text_area']
+        now = time.strftime("%c")
+        data = Data(subject=subject,text_data=text_data,date=str(now))
         data.save()
         return redirect("/submitted_data", code=302)
-    return render_template('app.html')
+    return render_template('app.html', text=text)
 
 @app.route('/submitted_data',methods=['GET', 'POST'])
 def submitted_data():
-    return render_template('submitted.html')
+    data = Data.objects
+    if request.method == 'POST':
+        subject = request.form['subject']
+        text_data = request.form['text_area']
+        data = Data(subject=subject,text_data=text_data)
+        data.save()
+    return render_template('submitted.html' ,text=data)
 
 
 @app.route('/logout',methods=['GET'])
@@ -47,12 +60,13 @@ def logout():
 
 
 class User(db.Document):
-    email = db.StringField(max_length=500)
-    password = db.StringField(max_length=1000)
+    email = db.StringField(max_length=50)
+    password = db.StringField(max_length=50)
 
 class Data(db.Document):
     subject = db.StringField(max_length=500)
     text_data = db.StringField(max_length=1000)
+    date = db.StringField(max_length=100)
 
 if __name__ == '__main__':
     app.run()
