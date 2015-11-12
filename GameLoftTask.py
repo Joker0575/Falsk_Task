05f73adc6c99c6cd56bc22e5f9738530bd02683e
Adroitent <raju.bandari@adroitent.com>
2015-11-12 18:19:36 +0530

    login sessions added

--- GameLoftTask.py
+++ GameLoftTask.py
@@ -1,16 +1,15 @@
-from flask import Flask,redirect
+from flask import Flask,redirect,session
 from flask import render_template
 from flask import g
 from flask import request
 from flask import request, jsonify
 from flask.ext.mongoengine import MongoEngine
-from textblob import TextBlob
 import time
 
 
 app = Flask(__name__)
 
-
+# ************configuration with mongo database using mongo engine******
 app.config['MONGODB_SETTINGS'] = {
     'db': 'gomeloft_db',
     'host': 'localhost',
@@ -18,11 +17,14 @@ app.config['MONGODB_SETTINGS'] = {
 }
 
 db = MongoEngine(app)
+# **********************************************************************
 
+# ***********************actions****************************************
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
+        session['user_name'] = email
         password = request.form['password']
         user = User(email=email,password=password)
         user.save()
@@ -33,6 +35,8 @@ def login():
 @app.route('/application',methods=['GET', 'POST'])
 def application():
     text = Data.objects
+    if(request.method == 'GET') and (session['user_name'] is None):
+         return redirect("/", code=302)
     if request.method == 'POST':
         subject = request.form['subject']
         text_data = request.form['text_area']
@@ -40,26 +44,29 @@ def application():
         data = Data(subject=subject,text_data=text_data,date=str(now))
         data.save()
         return redirect("/submitted_data", code=302)
-    return render_template('app.html', text=text)
+    return render_template('app.html', text=text,user_name=session['user_name'])
 
 @app.route('/submitted_data',methods=['GET', 'POST'])
 def submitted_data():
     data = Data.objects
+    if(request.method == 'GET') and (session['user_name'] is None):
+         return redirect("/", code=302)
     if request.method == 'POST':
         subject = request.form['subject']
         text_data = request.form['text_area']
         now = time.strftime("%c")
         data = Data(subject=subject,text_data=text_data,date=str(now))
         data.save()
-    return render_template('submitted.html' ,text=data)
+    return render_template('submitted.html' ,text=data,user_name=session['user_name'])
 
 
 @app.route('/logout',methods=['GET'])
 def logout():
-      return "logout"
+    session.pop('user_name', None)
+    return redirect("/", code=302)
+# **********************************************************************
 
-
-
+# *********************models*******************************************
 class User(db.Document):
     email = db.StringField(max_length=50)
     password = db.StringField(max_length=50)
@@ -68,6 +75,8 @@ class Data(db.Document):
     subject = db.StringField(max_length=500)
     text_data = db.StringField(max_length=1000)
     date = db.StringField(max_length=100)
+# ***********************************************************************
 
 if __name__ == '__main__':
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
